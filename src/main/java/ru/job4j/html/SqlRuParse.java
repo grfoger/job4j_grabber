fb833fa22972dfeaf7f234db7d4f889b66538872
Arhip <grfoger@gmail.com>
2022-03-02 20:22:43 +0300

    2.3. Загрузка деталей поста. [#285212] - base

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,9 +4,16 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.model.Post;
+
+import java.io.IOException;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
+
+        System.out.println(getPost("https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t").toString());
+
+        /**
         String url = "https://www.sql.ru/forum/job-offers/";
         int pages  = 5;
         for (int i = 1; i <= pages; i++) {
@@ -20,5 +27,20 @@ public class SqlRuParse {
                 System.out.println(parent.child(5).text());
             }
         }
+         */
     }
+
+    public static Post getPost(String url) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        Element title = doc.selectFirst(".messageHeader");
+        Element description = title.parent().parent().child(1).child(1);
+
+        Post post = new Post();
+        post.setTitle(title.text());
+        post.setDescription(description.text());
+        post.setLink(url);
+        return post;
+    }
+
+
 }
