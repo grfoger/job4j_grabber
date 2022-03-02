c4062dcda460e3c6a7f4f86a7d73afa08a240000
Arhip <grfoger@gmail.com>
2022-03-02 12:25:43 +0300

    1.1. Job c параметрами [#260360] - init

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -4,6 +4,8 @@ import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.InputStream;
+import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -11,11 +13,19 @@ import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
+
     public static void main(String[] args) {
+
         try {
+            AlertRabbit ar = new AlertRabbit();
+            Connection connect = ar.init();
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            JobDataMap data = new JobDataMap();
+            data.put("connect", connect);
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
             SimpleScheduleBuilder times = simpleSchedule()
                     .withIntervalInSeconds(takeSeconds())
                     .repeatForever();
@@ -24,9 +34,27 @@ public class AlertRabbit {
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
+            Thread.sleep(10000);
+            scheduler.shutdown();
+        } catch (Exception se) {
             se.printStackTrace();
         }
+    }
+
+    public Connection init() {
+        Connection connect = null;
+        try {
+            takeSettings();
+            Class.forName(takeSettings().getProperty("driver-class-name"));
+            connect = DriverManager.getConnection(
+                    takeSettings().getProperty("url"),
+                    takeSettings().getProperty("username"),
+                    takeSettings().getProperty("password")
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connect;
     }
 
     private static Properties takeSettings() {
@@ -41,17 +69,29 @@ public class AlertRabbit {
     }
 
     private static int takeSeconds() {
-        try {
-            return Integer.parseInt(takeSettings().get("rabbit.interval").toString());
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("Wrong argument. Check properties settings.");
+        if (takeSettings().getProperty("rabbit.interval") == null) {
+            throw new NullPointerException("Exception: argument is null!");
         }
+        return Integer.parseInt(takeSettings().getProperty("rabbit.interval"));
     }
 
     public static class Rabbit implements Job {
+
+        public Rabbit() {
+            System.out.println("Rabbit dance here ...");
+        }
+
         @Override
         public void execute(JobExecutionContext context) throws JobExecutionException {
-            System.out.println("Rabbit runs here ...");
+            Connection connect = (Connection) context.getJobDetail().getJobDataMap().get("connect");
+            try (PreparedStatement statement =
+                         connect.prepareStatement("insert into rabbit(created_date) values (?)")) {
+                statement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
+                statement.execute();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
