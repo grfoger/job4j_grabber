01597aecaf39aebd1ee7fbfb698a726eaf4f2398
Arhip <grfoger@gmail.com>
2022-03-03 18:10:19 +0300

    5. PsqlStore [#285209] - init

---
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -0,0 +1,118 @@
+package ru.job4j.grabber;
+
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+import ru.job4j.html.SqlRuParse;
+import ru.job4j.quartz.AlertRabbit;
+
+import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements Store, AutoCloseable {
+
+    private Connection cnn;
+
+    public PsqlStore(Properties cfg) {
+        try {
+            Class.forName(cfg.getProperty("driver-class-name"));
+            cnn = DriverManager.getConnection(
+                    cfg.getProperty("url"),
+                    cfg.getProperty("username"),
+                    cfg.getProperty("password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        try (InputStream in = PsqlStore.class.getClassLoader().getResourceAsStream("schema.properties")) {
+            Properties cfg = new Properties();
+            cfg.load(in);
+            try (PsqlStore psqlStore = new PsqlStore(cfg)) {
+                Parse parser = new SqlRuParse(new SqlRuDateTimeParser());
+                String url = "https://www.sql.ru/forum/job-offers/";
+                int pages  = 5;
+                for (int i = 1; i <= pages; i++) {
+                    parser.list(url + i).forEach(x -> psqlStore.save(x));
+                }
+                System.out.println(psqlStore.findById(1));
+                psqlStore.getAll().forEach(System.out::println);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void save(Post post) {
+        try (PreparedStatement statement =
+                     cnn.prepareStatement("insert into schema(name, text, link, created) values (?, ?, ?, ?);",
+                             Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, post.getTitle());
+            statement.setString(2, post.getDescription());
+            statement.setString(3, post.getLink());
+            statement.setTimestamp(4, Timestamp.valueOf(post.getCreated()));
+            statement.execute();
+            try (ResultSet resultSet = statement.getGeneratedKeys()) {
+                if (resultSet.next()) {
+                    post.setId(resultSet.getInt(1));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        List<Post> items = new ArrayList<>();
+        try (PreparedStatement statement = cnn.prepareStatement("select * from schema;")) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    items.add(getPostFromResult(resultSet));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return items;
+    }
+
+    @Override
+    public Post findById(int id) {
+        Post postById = null;
+        try (PreparedStatement statement = cnn.prepareStatement("select * from schema where id = ?;")) {
+            statement.setInt(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    postById = getPostFromResult(resultSet);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return postById;
+    }
+
+    private Post getPostFromResult(ResultSet resultSet) throws SQLException {
+        return new Post(
+                resultSet.getInt("id"),
+                resultSet.getString("name"),
+                resultSet.getString("text"),
+                resultSet.getString("link"),
+                resultSet.getTimestamp("created").toLocalDateTime()
+        );
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (cnn != null) {
+            cnn.close();
+        }
+    }
+}
