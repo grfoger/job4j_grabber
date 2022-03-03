1dedb261456747b0cb99933a18cf06c4d703838a
Arhip <grfoger@gmail.com>
2022-03-03 17:56:07 +0300

    2.4. SqlRuParse [#285213 #271395] - corrected

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -15,33 +15,56 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SqlRuParse implements Parse {
+
+    private final DateTimeParser dateTimeParser;
+
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+
     public static void main(String[] args) throws Exception {
         String url = "https://www.sql.ru/forum/job-offers/";
-        Parse parser = new SqlRuParse();
+        Parse parser = new SqlRuParse(new SqlRuDateTimeParser());
         System.out.println(parser.list(url).toString());
     }
 
     @Override
-    public List<Post> list(String link) throws IOException {
+    public List<Post> list(String link) {
         List<Post> posts = new ArrayList<>();
-        Document doc = Jsoup.connect(link).get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            posts.add(detail(td.child(0).attr("href")));
+        try {
+            Document doc = Jsoup.connect(link).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Post post = detail(td.child(0).attr("href"));
+                boolean containsJava = post.getTitle().toLowerCase().contains("java");
+                boolean containsJS = post.getTitle().toLowerCase().contains("javascript");
+                boolean isOnlyJava = containsJava && !containsJS;
+                if (!isOnlyJava) {
+                    continue;
+                }
+                posts.add(post);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return posts;
     }
 
     @Override
-    public Post detail(String link) throws IOException {
-        Document doc = Jsoup.connect(link).get();
-        Element body = doc.selectFirst(".msgTable").child(0);
-        Element title = body.child(0);
-        Element description = body.child(1).child(1);
-        Element dateCreate = body.child(2).child(0);
-        String date = dateCreate.text().split("\\[")[0];
-        DateTimeParser parser = new SqlRuDateTimeParser();
-        LocalDateTime time = parser.parse(date.substring(0, date.length() - 1));
-        return new Post(title.text(), link, description.text(), time);
+    public Post detail(String link) {
+        Post post = null;
+        try {
+            Document doc = Jsoup.connect(link).get();
+            Element body = doc.selectFirst(".msgTable").child(0);
+            Element title = body.child(0);
+            Element description = body.child(1).child(1);
+            Element dateCreate = body.child(2).child(0);
+            String date = dateCreate.text().split("\\[")[0];
+            LocalDateTime time = dateTimeParser.parse(date.substring(0, date.length() - 1));
+            post = new Post(title.text(), link, description.text(), time);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return post;
     }
 }
